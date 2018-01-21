0a5a2deed1eca9cc9858e435e070f69222cfc134
Cale Bierman <cale-bierman@uiowa.edu>
2018-01-21 13:29:56 -0600

    old project got created in a weird state, recreating to fix issues

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# LunafestDybFrontend
+# LunafestDyb
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.5.
 
--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('lunafest-dyb-frontend App', () => {
+describe('lunafest-dyb App', () => {
   let page: AppPage;
 
   beforeEach(() => {
