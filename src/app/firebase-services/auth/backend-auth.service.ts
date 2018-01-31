77754fbe731c4229fa94c59fbdfd396ed3f6df46
apizzimenti <pizzimentianthony@gmail.com>
2018-01-31 01:21:14 -0600

    add functionality to retrieve users' uids

--- src/app/firebase-services/auth/backend-auth.service.ts
+++ src/app/firebase-services/auth/backend-auth.service.ts
@@ -4,15 +4,24 @@ import * as firebase from "firebase/app";
 
 @Injectable()
 export class BackendAuthService {
-
-  constructor(private afAuth: AngularFireAuth) { }
-
-  public attemptLogin(email: string, password: string): Promise<firebase.User> {
-    return this.afAuth.auth.signInWithEmailAndPassword(email, password);
-  }
-
-  public logout(): Promise<void> {
-    return this.afAuth.auth.signOut();
-  }
-
+    constructor(private afAuth: AngularFireAuth) { }
+    
+    public attemptLogin(email: string, password: string): Promise<firebase.User> {
+        return this.afAuth.auth.signInWithEmailAndPassword(email, password);
+    }
+    
+    public logout(): Promise<void> {
+        return this.afAuth.auth.signOut();
+    }
+    
+    public uniqueId (): Promise<string> {
+        return new Promise((resolve, reject) => {
+            this.afAuth.auth.onAuthStateChanged((user) => {
+                if (!user) reject(null);
+                else resolve(user.uid);
+            });
+        });
+    }
 }
+
+
