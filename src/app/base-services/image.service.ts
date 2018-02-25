4349354ffd4434eaa57ff47acb45b4cd19af44ec
apizzimenti <pizzimentianthony@gmail.com>
2018-02-24 21:04:20 -0600

    Initial method.

---
+++ src/app/base-services/image.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { AngularFireStorage } from "angularfire2/storage";
+import { Observable } from "rxjs";
+import "rxjs/add/operator/take";
+
+@Injectable()
+export class BaseImageService {
+    constructor (private storage: AngularFireStorage) { }
+
+    /**
+     * @author Anthony Pizzimenti
+     * @desc Stores an image and returns the file download url.
+     * @param {File} image  Image to be uploaded.
+     * @returns {Observable<string>}
+     */
+    public storeImage (image: File): Observable<string> {
+        let path = "images/" + image.name;
+        return this.storage.upload(path, image).downloadURL();
+    }
+}
