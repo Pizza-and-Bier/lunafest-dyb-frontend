c309361b3de14ebc09d0850fb5db644be4038da1
cbierman <cale-bierman@uiowa.edu>
2018-02-27 21:28:02 -0600

    adding fields from form to user model

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -2,6 +2,9 @@
 export class User {
     public following?: string[];
     public uid: string;
-    public preferences: Object;
+    public firstName: string;
+    public lastName: string;
+    public email: string;
+    public phone: string;
     public role: string = "anonymous";
 }
