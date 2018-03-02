d3eaaf81a61a5b6bcfcb5b247075b91bf4710825
cbierman <cale-bierman@uiowa.edu>
2018-03-01 21:17:50 -0600

    adding item delete functionality

--- src/app/edit-item/edit-item.component.ts
+++ src/app/edit-item/edit-item.component.ts
@@ -59,7 +59,9 @@ export class EditItemComponent implements OnInit {
     let dialogRef = this.dialog.open(ConfirmDialogComponent, {
       data: {
         title: "Cancel Edit",
-        content: "Are you sure you want to cancel editing this item? Any data you've entered will be lost."
+        content: "Are you sure you want to cancel editing this item? Any data you've entered will be lost.",
+        acceptText: "Leave",
+        cancelText: "Continue Editing"
       }
     });
     dialogRef.afterClosed().subscribe((result) => {
@@ -67,6 +69,27 @@ export class EditItemComponent implements OnInit {
         this.router.navigate(["/user/items/list"]);
       }
     });
+  }
+
+  public deleteItem(): void {
+    let dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        title: `Delete ${this.existingItem.name}`,
+        content: "Are you sure you want to delete this item? You won't be able to undo this action.",
+        acceptText: "Delete Item",
+        cancelText: "Cancel"
+      }
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.editItemService.deleteItem(this.existingItem.key).then(
+          (_) => {
+            this.router.navigate(["/user/items/list"]);
+          }
+        );
+      }
+    })
+    
   }
 
   private getItem(itemId: string): void {
